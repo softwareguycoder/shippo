5445b91b975487a8fbb631d42c96dfab34c4c9ea
softwareguycoder <softwareguycoder@gmail.com>
2019-05-31 17:38:27 -0400

    Removed the use of the print functions in SessionManager

--- managers/session_manager.py
+++ managers/session_manager.py
@@ -1,7 +1,6 @@
 from comm.socket_wrapper import SocketWrapper
 from common.shippo_symbols import PROTOCOL_HELO_COMMAND, PROTOCOL_QUIT_COMMAND, \
     PROTOCOL_LIST_COMMAND
-from common.utilities import Utilities
 
 
 class SessionManager(object):
@@ -13,30 +12,22 @@ class SessionManager(object):
         if not self.__session:
             return False
         self.__session.Send(PROTOCOL_HELO_COMMAND)
-        Utilities.PrintTheStuffTheClientSent(PROTOCOL_HELO_COMMAND)
         response = self.__session.Receive()
-        Utilities.PrintTheStuffTheServerRepliedWith(response)
         return response.startswith("200 OK.")
             
     def ListRemoteProcesses(self):
         if not self.__session:
             return False
         self.__session.Send(PROTOCOL_LIST_COMMAND)
-        Utilities.PrintTheStuffTheClientSent(PROTOCOL_LIST_COMMAND)
         response = self.__session.Receive()
-        Utilities.PrintTheStuffTheServerRepliedWith(response)
         self.__responseLines = self.__session.ReceiveAllLines()
-        for strCurLine in self.__responseLines:
-            Utilities.PrintTheStuffTheServerRepliedWith(strCurLine)
         return response.startswith("204")
             
     def Close(self):
         if not self.__session:
             return False
         self.__session.Send(PROTOCOL_QUIT_COMMAND)
-        Utilities.PrintTheStuffTheClientSent(PROTOCOL_QUIT_COMMAND)
         response = self.__session.Receive()
-        Utilities.PrintTheStuffTheServerRepliedWith(response)
         result = response.startswith("206 OK.")
         self.__session.Close()
         return result
