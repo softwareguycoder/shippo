7af95c08d3b5e2d1c2d783c9c65016cd728aab1a
softwareguycoder <softwareguycoder@gmail.com>
2019-05-31 16:05:53 -0400

    Made program fully object-oriented

---
+++ comm/socket_wrapper.py
@@ -0,0 +1,87 @@
+from shippo_symbols import ERROR_FAILED_CONNECT_TO_SERVER_FORMAT, \
+    EXIT_FAILURE, ASCII_ENCODING, ERROR_FAILED_SEND_MESSAGE_FORMAT, \
+    BUFFER_SIZE, ERROR_FAILED_RECEIVE_FROM_SERVER_FORMAT
+import socket
+
+
+class SocketWrapper(object):
+
+    def __DoReceive(self):
+        strReply = ''
+        if self.__clientSocket is None:
+            return strReply
+        try:
+            strReply = self.__clientSocket.recv(BUFFER_SIZE) \
+                .decode(ASCII_ENCODING).strip()
+        except Exception as e:
+            print(ERROR_FAILED_RECEIVE_FROM_SERVER_FORMAT.format(e))
+            strReply = ''
+        
+        return strReply
+       
+    def __DoSend(self, bytesToSend):
+        result = 0  # bytes sent
+        if (len(bytesToSend) == 0):
+            return result
+        if self.__clientSocket is None:
+            return result
+        try:
+            self.__clientSocket.sendall(bytesToSend)
+            result = len(bytesToSend)
+        except:
+            result = 0
+    
+        return result
+
+    def __DoConnect(self, hostname, port):
+        result = False
+        if self.__clientSocket is None:
+            return result
+        try:
+            self.__clientSocket.connect((hostname, port))
+        except Exception as e:
+            print(ERROR_FAILED_CONNECT_TO_SERVER_FORMAT.format(e))
+            result = False
+        else:
+            result = True
+            
+        return result
+    
+    def Receive(self):
+        strReply = self.__DoReceive()
+        if len(strReply.strip()) == 0:
+            self.Close()
+            exit(EXIT_FAILURE)
+            
+        return strReply
+    
+    def Send(self, strMessage):
+        bytesSent = 0
+        if len(strMessage.strip()) == 0:
+            return bytesSent
+        bytesSent = self.__DoSend(strMessage.encode(ASCII_ENCODING))
+        if bytesSent <= 0:
+            self.Close()
+            print(ERROR_FAILED_SEND_MESSAGE_FORMAT.
+                  format(strMessage))
+            exit(EXIT_FAILURE)
+            
+        return bytesSent
+    
+    def Close(self):
+        if self.__clientSocket is None:
+            return
+        self.__clientSocket.close()
+        self.__clientSocket = None
+        pass
+
+    def Connect(self, hostname, port):
+        result = self.__DoConnect(hostname, port)
+        if not result:
+            self.Close()
+            exit(EXIT_FAILURE)
+        pass
+
+    def __init__(self):
+        self.__clientSocket = \
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- shippo.py
+++ shippo.py
@@ -1,83 +1,24 @@
 #!/usr/bin/python3
 
-import socket
-
-ASCII_ENCODING = 'ascii'
-BUFFER_SIZE = 1024
-BYTES_SENT_FORMAT = "B sent."
-ERROR_FAILED_SEND_HELO_COMMAND = \
-    "ERROR: Failed to send HELO command to the server."
-ERROR_FAILED_SEND_QUIT_COMMAND = \
-    "ERROR: Failed to send QUIT command to the server."
-EXIT_FAILURE = -1
-EXIT_SUCCESS = 0
-PROTOCOL_HELO_COMMAND = "HELO\n".encode(ASCII_ENCODING)
-PROTOCOL_QUIT_COMMAND = "QUIT\n".encode(ASCII_ENCODING)
-TCP_IP = '127.0.0.1'
-TCP_PORT = 9000
-SERVER_DATA_FORMAT = "S: {}"
-
-
-def DoConnect(clientSocket):
-    result = False
-    if clientSocket is None:
-        return result
-    try:
-        clientSocket.connect((TCP_IP, TCP_PORT))
-    except Exception as e:
-        print("ERROR: Failed to connect to server.\n", e)
-        result = False
-    else:
-        result = True
-        
-    return result
-
-
-def DoSend(clientSocket, bytesToSend):
-    result = 0  # bytes sent
-    if (len(bytesToSend) == 0):
-        return result
-    try:
-        result = clientSocket.send(bytesToSend)
-    except:
-        result = 0
-
-    return result
+from common.utilities import Utilities
+from comm.socket_wrapper import SocketWrapper
+from shippo_symbols import TCP_IP, TCP_PORT, \
+    PROTOCOL_HELO_COMMAND, PROTOCOL_QUIT_COMMAND, EXIT_SUCCESS
 
 
 def main():
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if not DoConnect(clientSocket):
-        exit(EXIT_FAILURE)
-    bytesSent = DoSend(clientSocket, PROTOCOL_HELO_COMMAND)
-    if bytesSent == 0:
-        print(ERROR_FAILED_SEND_HELO_COMMAND)
-        clientSocket.close()
-        exit(EXIT_FAILURE)
-        
-    print(bytesSent, BYTES_SENT_FORMAT)
-    receivedData = clientSocket.recv(BUFFER_SIZE).decode(ASCII_ENCODING).strip()
-    receivedDataLen = len(receivedData)
+    clientSocket = SocketWrapper()
+    clientSocket.Connect(TCP_IP, TCP_PORT)
     
-    print(SERVER_DATA_FORMAT.format(
-        receivedDataLen,
-        receivedData))
+    clientSocket.Send(PROTOCOL_HELO_COMMAND)
+    Utilities.PrintTheStuffTheClientSent(PROTOCOL_HELO_COMMAND)        
+    Utilities.PrintTheStuffTheServerRepliedWith(clientSocket.Receive())
     
-    bytesSent = DoSend(clientSocket, PROTOCOL_QUIT_COMMAND)
-    if bytesSent == 0:
-        print(ERROR_FAILED_SEND_QUIT_COMMAND)
-        clientSocket.close()
-        exit(EXIT_FAILURE)
+    clientSocket.Send(PROTOCOL_QUIT_COMMAND)
+    Utilities.PrintTheStuffTheClientSent(PROTOCOL_QUIT_COMMAND)        
+    Utilities.PrintTheStuffTheServerRepliedWith(clientSocket.Receive())
     
-    print(bytesSent, BYTES_SENT_FORMAT)
-    receivedData = clientSocket.recv(BUFFER_SIZE).decode(ASCII_ENCODING).strip()
-    receivedDataLen = len(receivedData)
-    
-    print(SERVER_DATA_FORMAT.format(
-        receivedDataLen,
-        receivedData))
-    
-    clientSocket.close()        
+    clientSocket.Close()        
     exit(EXIT_SUCCESS)
 
     
