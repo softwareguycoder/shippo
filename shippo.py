266c567396a3dfe2016064be83c96153c23a1c7d
softwareguycoder <softwareguycoder@gmail.com>
2019-05-31 17:33:03 -0400

    Added accessor to the SessionManager for the latest response lines

--- shippo.py
+++ shippo.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 from managers.session_manager import SessionManager
-from common.shippo_symbols import TCP_IP, TCP_PORT, EXIT_SUCCESS
+from common.shippo_symbols import TCP_IP, TCP_PORT, EXIT_SUCCESS, EXIT_FAILURE
+from common.utilities import Utilities
 
 def main():
     with SessionManager(TCP_IP, TCP_PORT) as session:
-        session.Open()
-        session.ListRemoteProcesses()
-    
+        if not session.Open():
+            exit(EXIT_FAILURE)
+        if not session.ListRemoteProcesses():
+            exit(EXIT_FAILURE)
+        responseLines = session.GetResponseLines()
+        for line in responseLines:
+            Utilities.PrintTheStuffTheServerRepliedWith(line)
+                
     exit(EXIT_SUCCESS)
 
     
