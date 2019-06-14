6bd828974059e4775a098e9fb3349934efec67d4
softwareguycoder <softwareguycoder@gmail.com>
2019-06-14 11:42:33 -0400

    Added ReplaceFromRight method

--- common/string_utils.py
+++ common/string_utils.py
@@ -7,4 +7,18 @@ class StringUtilities(object):
             # assume value is a string
             return value == '' or len(value.strip()) == 0
                 
-            
+        @staticmethod
+        def ReplaceFromRight(strValue, strFindWhat, 
+            strReplaceWith, nOccurrences):
+            if not strValue or not len(strValue.strip()):
+                return strValue
+            if not strFindWhat or not isinstance(strFindWhat, str):
+                return strValue
+            if not strReplaceWith or not isinstance(strReplaceWith, str):
+                return strValue
+            if not nOccurrences or not isinstance(nOccurrences, int):
+                return strValue
+            if nOccurrences <= 0:
+                return strValue
+            partsList = strValue.rsplit(strFindWhat, nOccurrences)
+            return strReplaceWith.join(partsList)
